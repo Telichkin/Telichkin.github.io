39a8e76e7124d7aa867aa35b629cf8eea9f94dec
Roman Telichkin <roman@telichk.in>
2018-07-12 22:16:12 +0300

    Move meta info into template

--- template-index.js
+++ template-index.js
@@ -1,9 +1,28 @@
-fetch('./posts/_meta.json')
-  .then(res => res.json())
-  .then(meta => {
-    const posts = meta.reverse().map(({ title, slug }, index) => e('li', { className: 'posts__post' }, 
-      e('a', { className: 'posts__link', href: `/${slug}`, textContent: `${index + 1}. ${title}` }),
-    ));
+const sMeta = [
+  {
+    title: 'Тесты на одном уровне абстракции',
+    slug: 'one-level-abstraction-tests',
+  },
+  {
+    title: 'Интеграционные или модульные тесты?',
+    slug: 'integration-or-unit-tests',
+  },
+  {
+    title: 'Невозможно разрабатывать без тестов',
+    slug: 'u-can-not-to-code-without-tests',
+  },
+  {
+    title: 'Какой у вас опыт работы по Agile?',
+    slug: 'agile-experience',
+  },
+  {
+    title: 'Вакансии',
+    slug: 'job-opportunities',
+  }
+]
 
-    e('ol', { className: 'posts' }, ...posts.reverse()).appendToNode(document.querySelector('.main'))
-  })
+const sPosts = sMeta.reverse().map(({ title, slug }, index) => e('li', { className: 'posts__post' }, 
+  e('a', { className: 'posts__link', href: `/${slug}`, textContent: `${index + 1}. ${title}` }),
+))
+
+e('ol', { className: 'posts' }, ...sPosts.reverse()).appendToNode(document.querySelector('.main'))
