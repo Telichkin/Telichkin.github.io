d7939aeef1c07493024e7012135097f3acaed5f7
Roman Telichkin <roman@telichk.in>
2018-10-30 10:09:58 +0300

    Replace "." prefix to "_"

--- build.js
+++ build.js
@@ -4,13 +4,13 @@ const path = require('path')
 
 mustache.escape = value => value
 
-const rawPostsDir = './.raw'
+const rawPostsDir = './_raw'
 const builtPostsDir = './notes'
 fs.readdirSync(rawPostsDir).forEach(fileName => {
   const rawPostPath = path.join(rawPostsDir, fileName)
   const rawPost = fs.readFileSync(rawPostPath)
 
-  const template = fs.readFileSync('./.blocks/post.html').toString()
+  const template = fs.readFileSync('./_blocks/post.html').toString()
 
   const builtPostContent = mustache.render(template, { main: rawPost })
   const builtPostPath = path.join(builtPostsDir, fileName)
